bf739413ae28a5a69d495b12a0edd108ad21bc01
imxblee <transcendencez@gmail.com>
2016-02-27 09:02:11 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixer5::Application.config.secret_key_base = 'c8412d01acd82be9c769e4bf55f05179a32c9c075fc275621c6d22bbb0898197b0dfbd1fe7fdfd0846477e1d98101c33cc4e2b91fead74a27dcdedd35bb3c75b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flixer5::Application.config.session_store :cookie_store, key: '_flixer5_session'
