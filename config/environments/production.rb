0eaec1ebf874e01615dd22a204dd3cf651231374
imxblee <transcendencez@gmail.com>
2016-02-27 09:54:15 +0000

    set up devise and create user model and signin/signout/login link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Flixer5::Application.configure do
+  config.action_mailer.default_url_options = { host: 'flixer5-xb.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
